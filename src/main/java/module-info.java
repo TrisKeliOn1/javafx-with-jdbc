923ee5c88d67429ae7d54de7083342e7585ac3bf
Hebert <hebertLandim007@outlook.com>
2024-07-29 09:34:10 -0300

    Main View

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,6 @@ module com.project.javafxwithjdbc {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.project.javafxwithjdbc to javafx.fxml;
     exports com.project.javafxwithjdbc;
-    exports com.project.javafxwithjdbc.Controllers;
-    opens com.project.javafxwithjdbc.Controllers to javafx.fxml;
 }
