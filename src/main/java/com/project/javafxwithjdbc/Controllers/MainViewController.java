b9d47888aef87cd7a99e0226eff0c67383c5e9bc
Hebert <hebertLandim007@outlook.com>
2024-07-30 10:09:41 -0300

    DepartmentService

--- src/main/java/com/project/javafxwithjdbc/Controllers/MainViewController.java
+++ src/main/java/com/project/javafxwithjdbc/Controllers/MainViewController.java
@@ -1,13 +1,14 @@
 package com.project.javafxwithjdbc.Controllers;
 
 import com.project.javafxwithjdbc.Main;
+import com.project.javafxwithjdbc.model.services.DepartmentService;
 import com.project.javafxwithjdbc.utils.Alerts;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
@@ -34,7 +35,7 @@ public class MainViewController implements Initializable {
 
     @FXML
     public void onMenuItemDepartmentAction() {
-        loadView("/FXML/DepartmentList.fxml");
+        loadView2("/FXML/DepartmentList.fxml");
     }
 
     @FXML
@@ -60,7 +61,30 @@ public class MainViewController implements Initializable {
             mainVBox.getChildren().addAll(newVBox.getChildren());
         }
         catch (IOException e) {
-            Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+            Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
         }
     }
+
+    private synchronized void loadView2(String absoluteName) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            VBox newVBox = loader.load();
+
+            Scene mainScene = Main.getMainScene();
+            VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+            Node mainMenu = mainVBox.getChildren().get(0);
+            mainVBox.getChildren().clear();
+            mainVBox.getChildren().add(mainMenu);
+            mainVBox.getChildren().addAll(newVBox.getChildren());
+
+            DepartmentListController controller = loader.getController();
+            controller.setDepartmentService(new DepartmentService());
+            controller.updateTableView();
+        }
+        catch (IOException e) {
+            Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+        }
+    }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,9 @@ module com.project.javafxwithjdbc {
     requires javafx.fxml;
 
     opens com.project.javafxwithjdbc to javafx.fxml;
-    exports com.project.javafxwithjdbc;
-
     opens com.project.javafxwithjdbc.Controllers to javafx.fxml;
+    opens com.project.javafxwithjdbc.model.entities to javafx.base; 
+
+    exports com.project.javafxwithjdbc;
     exports com.project.javafxwithjdbc.Controllers;
 }
